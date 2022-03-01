b3f61e2e750be04193acbc1cfea145f460811166
LearnToCode1138 <mdelgado1138@hotmail.com>
2022-02-28 20:30:05 -0800

    add CORS fix to app.js

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Request-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  next();
+});
+
 app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
 
@@ -27,7 +37,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    'mongodb+srv://mars:n3fRS3MdwkOxMti9@cluster0.db63l.mongodb.net/places?retryWrites=true&w=majority'
+    'mongodb+srv://mars:n3fRS3MdwkOxMti9@cluster0.db63l.mongodb.net/mern?retryWrites=true&w=majority'
   )
   .then(() => {
     app.listen(5000);
